196d7a21ad4b00f4433378454890ad31f4c2c7a6
frank boakye yiadom <fbyiadom@gmail.com>
2017-03-23 00:12:53 -0400

    added friendship controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
 	 root 'welcome#index'
+	 get 'my_friends', to: 'users#my_friends'
 	 get 'my_portfolio', to: "users#my_portfolio"
 	 get 'search_stocks', to: "stocks#search"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
