d7449c6d3a3666b0d2e769a75fc37717a70e132e
frank boakye yiadom <fbyiadom@gmail.com>
2017-03-24 23:07:52 -0400

    contact and about pages were added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :user_stocks, except: [:show, :edit, :update]
   resources :users, only: [:show]
   resources :friendships
+  resources :contacts
+  resources :abouts
 	 root 'welcome#index'
 	 get 'my_friends', to: 'users#my_friends'
 	 get 'my_portfolio', to: "users#my_portfolio"
